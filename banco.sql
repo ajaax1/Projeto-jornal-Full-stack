780f0aa0fe5c6b45e2b09231a66a7c16354834d7
Ruan Higor <ruanhigor123@gmail.com>
2023-12-05 12:20:22 -0300

    Colocando o painel adm

--- banco.sql
+++ banco.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 04-Set-2023 às 18:25
+-- Tempo de geração: 05-Dez-2023 às 15:18
 -- Versão do servidor: 8.0.31
--- versão do PHP: 8.1.13
+-- versão do PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,6 +31,7 @@ DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
   `paragraphs1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `paragraphs2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `paragraphs3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
@@ -43,7 +44,14 @@ CREATE TABLE IF NOT EXISTS `news` (
   `id_user` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `slug`, `paragraphs1`, `paragraphs2`, `paragraphs3`, `paragraphs4`, `paragraphs5`, `paragraphs6`, `image`, `date`, `hour`, `id_user`) VALUES
+(1, 'Create NewsCreate News Create NewsCreate NewsCreate NewsCreate NewsCreate NewsCreate News', 'create-newscreate-news-create-newscreate-newscreate-newscreate-newscreate-newscreate-news', 'Create News\r\n Create News\r\nCreate News\r\nCreate News\r\nCreate News\r\n', '', '', '', '', '', '../assets/img/Honda-Civic-LX-2020-Frente.jpg', '2023-12-04', '12:26 PM', 1);
 
 -- --------------------------------------------------------
 
@@ -58,9 +66,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `name2` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(150) NOT NULL,
+  `createBy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `updateBy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UQ_Email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `name2`, `password`, `email`, `createBy`, `updateBy`) VALUES
+(1, 'Ruan', 'Higor', '$2y$10$.e0m1.t0BGzxKMTaHbqrmeN/UPyyhkWN2MbSSgHwQlGl3P87FukIO', 'ruanhigor123@gmail.com', '1', '1'),
+(4, 'Ruan', 'Silva', '$2y$10$C5ik7oTnAf2UrXg8JpJzWe5wQoQkFaqGwtDg6CQlbny3l/bEmJl5S', 'ruan_higor2012@hotmail.com', '1', 'null');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

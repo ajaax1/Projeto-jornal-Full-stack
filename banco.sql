2b774f618e0205921cb2b04987dd1dc8a2ab8c41
Ruan Higor <ruanhigor123@gmail.com>
2023-12-05 12:22:45 -0300

    Colocando o painel adm

--- banco.sql
+++ banco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 05-Dez-2023 às 15:18
+-- Tempo de geração: 05-Dez-2023 às 15:22
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -44,14 +44,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `id_user` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `news`
---
-
-INSERT INTO `news` (`id`, `title`, `slug`, `paragraphs1`, `paragraphs2`, `paragraphs3`, `paragraphs4`, `paragraphs5`, `paragraphs6`, `image`, `date`, `hour`, `id_user`) VALUES
-(1, 'Create NewsCreate News Create NewsCreate NewsCreate NewsCreate NewsCreate NewsCreate News', 'create-newscreate-news-create-newscreate-newscreate-newscreate-newscreate-newscreate-news', 'Create News\r\n Create News\r\nCreate News\r\nCreate News\r\nCreate News\r\n', '', '', '', '', '', '../assets/img/Honda-Civic-LX-2020-Frente.jpg', '2023-12-04', '12:26 PM', 1);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -70,15 +63,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `updateBy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UQ_Email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `user`
---
-
-INSERT INTO `user` (`id`, `name`, `name2`, `password`, `email`, `createBy`, `updateBy`) VALUES
-(1, 'Ruan', 'Higor', '$2y$10$.e0m1.t0BGzxKMTaHbqrmeN/UPyyhkWN2MbSSgHwQlGl3P87FukIO', 'ruanhigor123@gmail.com', '1', '1'),
-(4, 'Ruan', 'Silva', '$2y$10$C5ik7oTnAf2UrXg8JpJzWe5wQoQkFaqGwtDg6CQlbny3l/bEmJl5S', 'ruan_higor2012@hotmail.com', '1', 'null');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
